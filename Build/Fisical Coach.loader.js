1b79c841bc79cc5985afe1ca85955b6eaf2ec265
Muhammad Hassan Sajjad <hasan.sajjad@mangomango.io>
2026-05-12 15:47:51 +0500

    Added new changes

--- Build/Fisical Coach.loader.js
+++ Build/Fisical Coach.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 89706633,
+    wasmFileSize: 89711284,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
